999bdc4d3ebf51574089587b0b5b687cb3247014
Arunkumar Palaniappan <11667975+arunkumarpalaniappan@users.noreply.github.com>
2018-04-18 06:12:56 +0530

    Update Home&actions

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,33 @@ import { Grid, Input, Checkbox } from 'semantic-ui-react'
 import '../css/style.css';
 
 class Home extends Component {
+  constructor(props){
+      super(props);
+      this.state = {
+          todos: {
+              1:{
+                  name: 'Hello',
+                  status: false,
+                  key: 1
+              },
+              2:{
+                  name: 'World',
+                  status: true ,
+                  key: 2
+              }
+          }
+      };
+      this.updateTodo = this.updateTodo.bind(this);
+  }
+  updateTodo(event) {
+      console.log(event)
+  }
   render() {
+      const styles = {
+        todoDone: {
+            textDecoration:"line-through"
+            }
+      };
     return (
       <div className="todoHome">
           <Grid>
@@ -13,12 +39,17 @@ class Home extends Component {
                       placeholder='Add todo...' />
               </Grid.Column>
               <Grid.Column width={16}>
-                  <Checkbox label={{ children: 'Todo' }} /> <br/>
-                  <Checkbox label={{ children: 'Todo' }} /> <br/>
-                  <Checkbox label={{ children: 'Todo' }} /> <br/>
-                  <Checkbox label={{ children: 'Todo' }} /> <br/>
-                  <Checkbox label={{ children: 'Todo' }} /> <br/>
-                  <Checkbox label={{ children: 'Todo' }} /> <br/>
+                  {Object.values(this.state.todos).map(todo => {
+                      return <span key={todo.key}>
+                                    <Checkbox key={todo.key}
+                                              label={todo.name}
+                                              checked={todo.status}
+                                              style={todo.status?styles.todoDone:null}
+                                              onClick={this.updateTodo(todo.key,!todo.status)}
+                                              id={`todo${todo.key}`}  />
+                                    <br/>
+                            </span>
+                  })}
               </Grid.Column>
           </Grid>
       </div>
