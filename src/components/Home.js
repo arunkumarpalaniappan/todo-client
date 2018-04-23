b1e5e8e226dccf2e96755e9426457b7484790312
Arunkumar Palaniappan <11667975+arunkumarpalaniappan@users.noreply.github.com>
2018-04-23 21:16:51 +0530

    Updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,60 +1,66 @@
 import React, { Component } from 'react';
-import { Grid, Input, Checkbox } from 'semantic-ui-react'
-import '../css/style.css';
-
+import { Layout, Icon , Row, Col} from 'antd';
+const { Content } = Layout;
 class Home extends Component {
-  constructor(props){
-      super(props);
-      this.state = {
-          todos: {
-              1:{
-                  name: 'Hello',
-                  status: false,
-                  key: 1
-              },
-              2:{
-                  name: 'World',
-                  status: true ,
-                  key: 2
-              }
-          }
-      };
-      this.updateTodo = this.updateTodo.bind(this);
-  }
-  updateTodo(event) {
-      console.log(event)
-  }
-  render() {
-      const styles = {
-        todoDone: {
-            textDecoration:"line-through"
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: {
+                1: {
+                    name: 'Hello',
+                    status: false,
+                    key: 1
+                },
+                2: {
+                    name: 'World',
+                    status: true,
+                    key: 2
+                }
             }
-      };
-    return (
-      <div className="todoHome">
-          <Grid>
-              <Grid.Column width={16}>
-                  <Input
-                      fluid
-                      placeholder='Add todo...' />
-              </Grid.Column>
-              <Grid.Column width={16}>
-                  {Object.values(this.state.todos).map(todo => {
-                      return <span key={todo.key}>
-                                    <Checkbox key={todo.key}
-                                              label={todo.name}
-                                              checked={todo.status}
-                                              style={todo.status?styles.todoDone:null}
-                                              onClick={this.updateTodo(todo.key,!todo.status)}
-                                              id={`todo${todo.key}`}  />
-                                    <br/>
-                            </span>
-                  })}
-              </Grid.Column>
-          </Grid>
-      </div>
-    );
-  }
+        };
+        this.updateTodo = this.updateTodo.bind(this);
+    }
+    updateTodo(event) {
+        console.log(event)
+    }
+    render() {
+        const styles = {
+            todoDone: {
+                textDecoration: "line-through"
+            }
+        };
+        return (
+            <div className="todoHome">
+                <Layout>
+                    <Content style={{ padding: '20px 5px' }}>
+                        <Layout style={{ padding: '24px 0', background: '#fff' }}>
+                            <Content style={{ padding: '0 24px', minHeight: 500 }}>
+                                <Row>
+                                    <Col span={2}>
+                                        <span>
+                                            <img src={"https://api.adorable.io/avatars/72/"+Date.now()+".png"} alt={"gravatar"}className={"profile-icon"} />
+                                        </span>
+                                    </Col>
+                                    <Col span={14}>
+                                    </Col>
+                                    <Col span={8}>
+                                        <Row>
+                                            <Col span={8}>
+                                            </Col>
+                                            <Col span={8}>
+                                            </Col>
+                                            <Col span={8}>
+                                            </Col>
+                                        </Row>
+                                    </Col>
+                                </Row>
+                            </Content>
+                        </Layout>
+                    </Content>
+                </Layout>
+            </div>
+        );
+    }
 }
 
 export default Home;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import Home from './components/Home';
 import registerServiceWorker from './registerServiceWorker';
+import 'antd/dist/antd.css';
+import './css/style.css';
 
 ReactDOM.render(<Home />, document.getElementById('root'));
 registerServiceWorker();
