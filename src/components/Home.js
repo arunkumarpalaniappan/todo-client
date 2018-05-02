e7c01ac2dc5d5a3205e80ac7780a13467a736b32
Arunkumar Palaniappan <11667975+arunkumarpalaniappan@users.noreply.github.com>
2018-05-02 06:29:07 +0530

    quotes Update

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,6 +18,29 @@ class Home extends Component {
                 }
             }
         };
+        Date.prototype.greet = () => {
+                let i = 0;
+                let hour = new Date().getHours();
+                let dayParts = [6,12,16,19,24];//Keep in this order
+                let greetings = [
+                    'You\'re up Early!',
+                    'Good Morning!',
+                    'Good Afternoon!',
+                    'Good Evening!',
+                    'Good Night!'
+                ];
+
+                let greet = () => {
+                    if( hour < dayParts[i] ) {
+                        return greetings[i];
+                    }
+                    else{
+                        i++;
+                        return greet();
+                    }
+                };
+                return greet();
+        };
         this.updateTodo = this.updateTodo.bind(this);
     }
     updateTodo(event) {
@@ -36,12 +59,9 @@ class Home extends Component {
                         <Layout style={{ padding: '24px 0', background: '#fff' }}>
                             <Content style={{ padding: '0 24px', minHeight: 500 }}>
                                 <Row>
-                                    <Col span={2}>
-                                        <span>
-                                            <img src={"https://api.adorable.io/avatars/72/"+Date.now()+".png"} alt={"gravatar"}className={"profile-icon"} />
-                                        </span>
-                                    </Col>
                                     <Col span={14}>
+                                        {new Date().greet()} ,
+
                                     </Col>
                                     <Col span={8}>
                                         <Row>
