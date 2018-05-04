08a275e28cbdfef072d9bfcc2c59173df079db71
Arunkumar Palaniappan <11667975+arunkumarpalaniappan@users.noreply.github.com>
2018-05-04 11:14:29 +0530

    config

--- src/api/quotesApi.js
+++ src/api/quotesApi.js
@@ -1,6 +1,8 @@
+const config = require('config');
+const host = `${config.quotes.ssl?'https://':'http://'}${config.quotes.host}:${config.quotes.port}/`
 class quotes {
     static qod() {
-        return fetch('https://quotes.rest/qod')
+        return fetch(`${host}qod`)
             .then((response) => response.json())
             .then((json) => json)
             .catch((ex) => ex)
